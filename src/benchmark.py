c0d6cafab59e962b6a5105e73ed70d51a4b410aa
Joxos <johndoe@example.com>
2023-08-27 17:11:55 +0800

    update benchmark.py

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,14 +1,14 @@
 import time
 from threading import Thread
 
-from client import main
+from client import main, server_address
 from utils import handle_run_main
 
 REQUEST_NUM = 1000
 
 if __name__ == '__main__':
     threads = [
-        Thread(target=handle_run_main, args=(main, ))
+        Thread(target=handle_run_main, args=(main, server_address))
         for _ in range(REQUEST_NUM)
     ]
     start = time.time()
