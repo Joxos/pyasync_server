846097c20377ef5b4ad87ebfbbd7bd139bcd112b
Joxos <johndoe@example.com>
2024-02-17 08:36:29 +0800

    update

--- pyasync_server/client/config.py
+++
@@ -1,2 +0,0 @@
-CRT_PATH = "server.crt"
-SERVER_ADDRESS = ("127.0.0.1", 1145)
--- run_client.py
+++ run_client.py
@@ -3,9 +3,10 @@ run_client.py: An example of how to run a client.
 """
 from pyasync_server.common.utils import handle_run_main, resolve_client_ssl_context
 from pyasync_server.client.package import pack_request_login
-from pyasync_server.client.config import SERVER_ADDRESS, CRT_PATH
 from pyasync_server.client.client import send_simple_package
 
+CRT_PATH = "server.crt"
+SERVER_ADDRESS = ("127.0.0.1", 1145)
 
 
 async def main():
